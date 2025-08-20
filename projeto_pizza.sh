14507e9d369c674904067fde464fe4f064bd2f1b
packerzzz <115124886+BryanPacker@users.noreply.github.com>
2025-08-20 20:30:56 -0300

    Ultima alteração código

--- projeto_pizza.sh
+++ projeto_pizza.sh
@@ -18,7 +18,9 @@ elif [ ! -d "proway-docker/.git" ]; then
     cd proway-docker
 else
     cd proway-docker
-    git pull
+     # Use git reset --hard to ensure idempotency (restores deleted files)
+    git fetch origin
+    git reset --hard origin/main 2>/dev/null || git reset --hard origin/master
 fi
 
 # Kill processes running on ports 8080 and 5001
